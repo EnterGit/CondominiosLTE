00613ee8d38bf39957eedc3617a08320e276eb47
claudio_curin <ccurinmiranda@gmail.com>
2023-12-12 10:23:51 -0300

    app module ccurin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -105,14 +105,9 @@ registerLocaleData(localeEn, 'en-EN');
         CrudPropiedadComponent,
         CrudVehiculoComponent,
         CrudVisitasComponent,
-<<<<<<< HEAD
         CrudZonasComponent,
         ShowforrolesDirective,
         CrudCondominiosComponent
-=======
-         CrudZonasComponent,
-        ShowforrolesDirective
->>>>>>> b8cfe2eefdf1ad96222dbb34b03b0a61236634e1
     ],
     imports: [
         MatCardModule,
